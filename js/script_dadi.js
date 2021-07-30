d1f2a36c72dbdccdb7a3495443efc74beec23ab3
leonardobarone <leonardobarone26@gmail.com>
2021-07-30 17:51:14 +0200

    dadi visibile in html

--- js/script_dadi.js
+++ js/script_dadi.js
@@ -1,16 +1,17 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 var numeroComputer = parseInt(Math.random()*6)+1;
-document.getElementById('numeroComputer');
+document.getElementById('numeroComputer').innerHTML += numeroComputer;
 var numeroGiocatore = parseInt(Math.random()*6)+1;
-document.getElementById('numeroGiocatore').innerHTML;
+document.getElementById('numeroGiocatore').innerHTML += numeroGiocatore;
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (numeroGiocatore > numeroComputer) {
-    console.log('ha vinto il giocatore');
+    document.getElementById('Risultato').innerHTML += 'ha vinto il giocatore';  
 } else if (numeroGiocatore < numeroComputer) {
-    console.log('ha vinto il computer')
+    
+    document.getElementById('Risultato').innerHTML += 'ha vinto il computer';
 } else {
-    console.log('è patta!');
+    document.getElementById('Risultato').innerHTML += 'è patta!';
 }
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
