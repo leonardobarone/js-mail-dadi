ea3708696def8cb62691df5cf40b2fe0ab773dd2
leonardobarone <leonardobarone26@gmail.com>
2021-07-30 16:16:17 +0200

    esercizio mail

--- js/script_mail.js
+++ js/script_mail.js
@@ -1,4 +1,24 @@
-// Mail
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
+// MAIL
+
+// lista di email accettate
+var vecchieEmail = 
+[
+    "leonardo@gmail.com",
+    "samuele@gmail.com",
+    "manfredi@gmail.com",
+    "federico@gmail.com",
+    "gianluca@gmail.com",
+];
+// chiedi all'utente la sua email
+var nuovaEmail = prompt('Qual è la tua email?');
+// controlla che sia nella lista di chi può accedere 
+for (i = 0; i < vecchieEmail.length; i++) {
+    if (vecchieEmail[i] == nuovaEmail) {
+        document.getElementById('message').innerHTML = "L'email è già stata registrata in passato!";
+    } else {
+        document.getElementById('message').innerHTML = "L'email non è mai stata registrata! ISCRIVITI ADESSO!!!";
+    }
+}
 // stampa un messaggio appropriato sull’esito del controllo.
+
+
