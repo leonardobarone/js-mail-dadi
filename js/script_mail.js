06e549dd21b6437574e26b065f84f2145aa2263c
leonardobarone <leonardobarone26@gmail.com>
2021-07-31 16:43:10 +0200

    correzzione script mail

--- js/script_mail.js
+++ js/script_mail.js
@@ -1,7 +1,7 @@
 // MAIL
 
 // lista di email accettate
-var vecchieEmail = 
+var mails = 
 [
     "leonardo@gmail.com",
     "samuele@gmail.com",
@@ -10,18 +10,18 @@ var vecchieEmail =
     "gianluca@gmail.com",
 ];
 // chiedi all'utente la sua email
-var nuovaEmail = prompt('Qual è la tua email?');
-// controlla che sia nella lista di chi può accedere 
-var control = false;
-for (i = 0; i < vecchieEmail.length-1; i++) {
-    if (vecchieEmail[i] == nuovaEmail) {
+var mail = prompt('Qual è la tua email?');
+control = false;
+// controlla che sia nella lista di chi può accedere
+for (i = 0; i < mails.length; i++) {
+    if (mail == mails[i]) {
         control = true;
-    document.getElementById('message').innerHTML = "c'è";
-    } else {
-        control = false;
-        document.getElementById('message').innerHTML = "non c'è";
-    }
+        document.getElementById('message').innerHTML = "SIIIIIII";
+    } 
 }
+if ( control == false) {
+    document.getElementById('message').innerHTML = "NOOOOOOOOOOOOOO";
+} 
 // stampa un messaggio appropriato sull’esito del controllo.
 
 
